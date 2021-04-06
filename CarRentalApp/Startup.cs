07f906465ca4f803de8ed80532bf4708773f4698
Jerry <jerrytan93@hotmail.com>
2021-04-06 22:20:27 +0800

    First commit yay

---
+++ CarRentalApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarRentalApp.Startup))]
+namespace CarRentalApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
